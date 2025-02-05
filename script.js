2206dc69de2a7aff7a222bf128306bd63bc096d8
J-max <joelmtn7@gmail.com>
2025-02-05 23:11:33 +0100

    the array

--- script.js
+++ script.js
@@ -1,2 +1,34 @@
 const questionText = document.getElementById("question");
-questionText.innerText ="hiii"
+const popText = document.getElementById("qimg");
+const option1 = document.getElementById("opt1");
+const option2 = document.getElementById("opt2");
+const option3 = document.getElementById("opt3");
+
+ const previous = document.getElementById("prev");
+ const quit = document.getElementById("quit");
+ const next = document.getElementById("next");
+
+ let currentQuestion = 0;
+ let score = 0;
+
+const quizData = [
+    {question : "What is the capital of France",
+     options: ["paris", "london", "Abia"]   ,
+     answer: 0
+
+    },
+
+    {question : "Who Painted Mona Lisa",
+        options: ["Leonard Da Vinci", "Michealangelo", "Raphael"]   ,
+        answer: 0
+   
+       },
+
+       {question : "What class of food  is Beans",
+        options: ["calcium", "Carbohydrate", "Protein"]   ,
+        answer: 0
+   
+       }
+
+
+]
